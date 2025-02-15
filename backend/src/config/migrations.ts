d11920f095fb18e4db1a92ed0d0bdc4507d7fb0a
PETKO\petko <kirceto110@gmail.com>
2025-02-15 19:09:46 +0200

    added user id in migration of table

--- backend/src/config/migrations.ts
+++ backend/src/config/migrations.ts
@@ -4,6 +4,7 @@ const createNotesTable = async () => {
   const query = `
     CREATE TABLE IF NOT EXISTS notes (
       id SERIAL PRIMARY KEY,
+      user_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
       title VARCHAR(255) NOT NULL,
       content TEXT NOT NULL,
       created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
