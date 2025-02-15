8ef8c345943118357c58fc35fe9f5ef57747f415
PETKO\petko <kirceto110@gmail.com>
2025-02-15 16:32:26 +0200

    added cors so that the frontend can make requests to the backend without receiving cors error

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,11 +3,17 @@ import dotenv from "dotenv";
 import noteRoutes from "./routes/noteRoutes";
 import authRoutes from "./routes/authRoutes";
 import { setupSwagger } from "./config/swagger";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true,
+}));
 
 app.use(express.json());
 
